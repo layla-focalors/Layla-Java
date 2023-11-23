08f8d194df2062354478b0dbb3defdd697f86b04
arisia <starlights2283@gmail.com>
2023-11-23 13:53:05 +0900

    update prc.java

--- prc.java
+++ prc.java
@@ -1,6 +1,13 @@
 public class prc {
     public static void main(String[] args){
-        
+        String str1 = new String("layla");
+        String str2 = "layla";
+        // 메모리 위치가 달라 결과값아 다르다.
+        System.out.printf("%s == %s : %b\n", str1, str2, (str1 == str2));
+
+        String str3 = "Layla";
+        String str4 = "Layla";
+        System.out.printf("%s == %s : %b", str3, str4, (str3 == str4));
     }
 }
 
