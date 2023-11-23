46f9b3fe6329f9bab4adef604cf2c85326634946
arisia <starlights2283@gmail.com>
2023-11-23 14:59:11 +0900

    add new project files

---
+++ J_appPython/test.java
@@ -0,0 +1,9 @@
+package J_appPython;
+
+public class test {
+    public class TextInv {
+        app.python('import datetime')
+        app.python('x = datetime.datetime.now()')
+        app.python('print(x)')
+    }
+}
---
+++ function.java
@@ -0,0 +1,6 @@
+
+public class function {
+    public static void main(String[] args){
+        
+    }
+}
