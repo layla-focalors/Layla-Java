6af2141bff52693f86e0a10c2cbc8e4c4eefbed4
arisia <starlights2283@gmail.com>
2023-11-23 15:02:03 +0900

    add test.java

--- J_appPython/test.java
+++ J_appPython/test.java
@@ -5,5 +5,7 @@ public class test {
         app.python('import datetime')
         app.python('x = datetime.datetime.now()')
         app.python('print(x)')
+        app.save('./main.py')
+        app.python('for i in range(10):\n\tprint(i)')
     }
 }
