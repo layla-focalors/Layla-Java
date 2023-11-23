474c90f9a45b138dd4ef49efe35cd3fc6cda2541
arisia <starlights2283@gmail.com>
2023-11-23 14:50:49 +0900

    add iterator

--- ionc.java
+++ ionc.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-
+import java.util.Iterator;
 public class ionc {
     public static void main(String[] args){
         ArrayList<Integer> arr_list = new ArrayList<Integer>();
@@ -9,6 +9,11 @@ public class ionc {
 
         for(int i = 0; i<arr_list.size(); i++){
             System.out.println(arr_list.get(i));
+        }
+
+        Iterator<Integer> ir = arr_list.iterator();
+        while(ir.hasNext()){
+            System.out.println(ir.next());
         }
     }
 }
