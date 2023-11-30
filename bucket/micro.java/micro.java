16cda0274b63421e91026716efe0e579369eff37
arisia <starlights2283@gmail.com>
2023-11-30 14:18:38 +0900

    add implements method

--- bucket/micro.java/micro.java
+++ bucket/micro.java/micro.java
@@ -2,18 +2,29 @@
  * Cardable
  */
 interface Cardable {
-    int payCard(String payment_title, int payment); 
+    public abstract int payCard(String payment_title, int payment); 
     // 카드 결제 메서드
-    int payTrans(int fee);
+    public abstract int payTrans(int fee);
     // 교통카드 결제 메서드
-    void printCardInfo();
+    public abstract void printCardInfo();
     // 카드의 정보 출력
-    void printCardHistory();
+    public abstract void printCardHistory();
     // 카드 사용 내역 출력 메서드
 }
 
 class Card implements Cardable {
-
+    public int payCard(String payment_title, int payment){
+        return 0;
+    }
+    // 카드 결제 메서드
+    public int payTrans(int fee) {
+        return 0;
+    }
+    public void printCardInfo(){
+    }
+    // 카드의 정보 출력
+    public void printCardHistory(){
+    }
 }
 
 class CreditCard extends Card {
@@ -21,6 +32,12 @@ class CreditCard extends Card {
 class CheckCard extends Card {
 }
 
+class CardManager {
+
+}
+
 public class micro {
-    
+    public static void main(String[] args){
+
+    }
 }
